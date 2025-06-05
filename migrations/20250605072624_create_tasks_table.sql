dff10159804d62d30359fc5a639f6176f987dcff
Sherwin Bularon <71875244+sherwinbularon@users.noreply.github.com>
2025-06-05 16:47:37 +0800

    Delete migrations/20250605072624_create_tasks_table.sql

--- migrations/20250605072624_create_tasks_table.sql
+++
@@ -1,7 +0,0 @@
--- Add migration script here
-CREATE TABLE IF NOT EXISTS tasks (
-    id UUID PRIMARY KEY,
-    title TEXT NOT NULL,
-    completed BOOLEAN DEFAULT false,
-    created_at TIMESTAMP DEFAULT now()
-);
